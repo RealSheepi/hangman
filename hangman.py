a75b0a1d0a5a54b4fbd52e251b98f1c1eb46dc7c
RealSheepi <realsheep1234@gmail.com>
2024-07-11 14:54:43 +0530

    main file

---
+++ hangman.py
@@ -0,0 +1,52 @@
+import random
+
+def choose_word():
+    words = ["apple", "blimp", "brown", "grain", "mimic", "strawberry", "peach", "pineapple", "strange"]
+    return random.choice(words)
+
+def display_word(word, guessed_letters):
+    display = ""
+    for letter in word:
+        if letter in guessed_letters:
+            display += letter + " "
+        else:
+            display += "_ "
+    return display.strip()
+
+def hangman():
+    print("Welcome to Hangman!")
+
+    word = choose_word()
+    guessed_letters = []
+    attempts_left = 6
+
+    while attempts_left > 0:
+        print("\n" + display_word(word, guessed_letters))
+        print(f"Attempts left: {attempts_left}")
+
+        guess = input("Guess a letter: ").lower()
+
+        if len(guess) != 1 or not guess.isalpha():
+            print("Please enter only 1 letter.")
+            continue
+
+        if guess in guessed_letters:
+            print("You already guessed that letter.")
+            continue
+
+        guessed_letters.append(guess)
+
+        if guess in word:
+            print("Good guess!")
+        else:
+            print("That letter is not in the word.")
+            attempts_left -= 1
+
+        if all(letter in guessed_letters for letter in word):
+            print("\n" + display_word(word, guessed_letters))
+            print("You guessed it:", word)
+            return
+
+    print("You ran out of attempts.It was:", word)
+
+hangman()
